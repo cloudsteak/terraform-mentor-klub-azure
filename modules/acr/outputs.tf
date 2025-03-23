aae6733e060bd5ab7dde53ecf656d1e7b0495ec6
the1bit <tibor.kiss@cloudmentor.hu>
2025-03-23 11:07:38 +0100

    TODO

--- modules/acr/outputs.tf
+++ modules/acr/outputs.tf
@@ -6,5 +6,5 @@ output "acr_login_server" {
 }
 
 output "todo_message" {
-  value = "TODO: You must create Azure DevOps Service Conection manually for the Azure Container Registry (ACR) to enable CI/CD pipeline. Or add it cloudsteak-MentorKlub-f96295a4-0752-4228-ad35-419f404433a0 service principal with ACRPush role."
+  value = "TODO: You must create Azure DevOps Service Conection manually for the Azure Container Registry (ACR) to enable CI/CD pipeline. Or add the right service principal to it with ACRPush role."
 }
